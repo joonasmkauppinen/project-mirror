e26663f59107c274c95689045aa15fe3f109f03e
Joonas Kauppinen <joonasm.kauppinen@gmail.com>
2019-10-27 20:49:40 +0200

    Add: header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Header.module.css';
+
+const Header: React.FC = ({ children }) => (
+  <p className={styles.header}>{children}</p>
+);
+
+Header.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+};
+
+export default Header;
