299c4ccdefce90d35a3107dd14d01fdfe4c976a1
Joonas Kauppinen <joonasm.kauppinen@gmail.com>
2019-10-27 16:58:35 +0200

    Update: App component to use typescript

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Mirror mirror on the wall.
-        </p>
-        <a
-          className="App-link"
-          href="https://images.google.com/search?q=mirror"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Google for Mirrors
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = (): JSX.Element => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>Mirror mirror on the wall.</p>
+      <a
+        className="App-link"
+        href="https://images.google.com/search?q=mirror"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Google for Mirrors
+      </a>
+    </header>
+  </div>
+);
 
 export default App;
