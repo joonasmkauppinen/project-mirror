735b19ebe63b0649a6cac065924cb7099598b8a8
Joonas Kauppinen <joonasm.kauppinen@gmail.com>
2019-10-27 21:07:14 +0200

    Add: basic routing between pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,34 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import LandingPage from '../pages/LandingPage';
 import MainAppPage from '../pages/MainAppPage';
 
-const App = (): JSX.Element => <MainAppPage />;
+const App = (): JSX.Element => (
+  <Router>
+    <div>
+      {/* TODO: remove sample navigation */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Landing Page</Link>
+          </li>
+          <li>
+            <Link to="/main">Main App</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route path="/main">
+          <MainAppPage />
+        </Route>
+        <Route path="/">
+          <LandingPage />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;
