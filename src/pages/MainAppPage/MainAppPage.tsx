dc868208a295cabca3638b6ba8270c75126bba59
Niklas Kiuru <niklas.kiuru1@gmail.com>
2019-10-28 19:32:07 +0200

    Add: nested routes for different tabs. Closes #10

--- src/pages/MainAppPage/MainAppPage.tsx
+++ src/pages/MainAppPage/MainAppPage.tsx
@@ -1,13 +1,51 @@
 import React, { FC } from 'react';
-import Header from '../../components/Header';
-import Text from '../../components/Text';
 import styles from '../PageContainer.module.css';
+import { Route, Switch, useRouteMatch } from 'react-router';
+import HomeTab from '../HomeTab';
+import DiscoverTab from '../DiscoverTab';
+import ChatTab from '../ChatTab';
+import ProfileTab from '../ProfileTab';
+import { Link } from 'react-router-dom';
 
-const MainAppPage: FC = () => (
-  <div className={styles.container}>
-    <Header>Main app page</Header>
-    <Text>App tabs will be on this page.</Text>
-  </div>
-);
+const MainAppPage: FC = () => {
+  const match = useRouteMatch();
+  if (!match) {
+    return <div>Error</div>;
+  }
+  return (
+    <div className={styles.container}>
+      <nav>
+        <ul>
+          <li>
+            <Link to={`${match.url}/home`}>Home</Link>
+          </li>
+          <li>
+            <Link to={`${match.url}/discover`}>Discover</Link>
+          </li>
+          <li>
+            <Link to={`${match.url}/chat`}>Chat</Link>
+          </li>
+          <li>
+            <Link to={`${match.url}/profile`}>Profile</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path={`${match.path}/home`}>
+          <HomeTab />
+        </Route>
+        <Route path={`${match.url}/discover`}>
+          <DiscoverTab />
+        </Route>
+        <Route path={`${match.url}/chat`}>
+          <ChatTab />
+        </Route>
+        <Route path={`${match.url}/profile`}>
+          <ProfileTab />
+        </Route>
+      </Switch>
+    </div>
+  );
+};
 
 export default MainAppPage;
