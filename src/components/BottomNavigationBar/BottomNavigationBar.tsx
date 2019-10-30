7399a9b81204f4de2da262fdf7c1d82bcbd9ac8e
Niklas Kiuru <niklas.kiuru1@gmail.com>
2019-10-30 18:46:23 +0200

    Close #22. Improvements to tab bar -Add filled icons -Update outlined icons -Position tab bar correctly

--- src/components/BottomNavigationBar/BottomNavigationBar.tsx
+++ src/components/BottomNavigationBar/BottomNavigationBar.tsx
@@ -1,26 +1,42 @@
 import React, { FunctionComponent } from 'react';
 import styles from './BottomNavigationBar.module.css';
-import { ReactComponent as HomeIcon } from '../../assets/home.svg';
-import { ReactComponent as DiscoverIcon } from '../../assets/discover.svg';
-import { ReactComponent as ChatIcon } from '../../assets/chat.svg';
-import { ReactComponent as ProfileIcon } from '../../assets/profile.svg';
+import { ReactComponent as HomeIcon } from '../../assets/outlined/home.svg';
+import { ReactComponent as HomeIconActive } from '../../assets/filled/home-filled.svg';
+import { ReactComponent as DiscoverIcon } from '../../assets/outlined/discover.svg';
+import { ReactComponent as DiscoverIconActive } from '../../assets/filled/discover-filled.svg';
+import { ReactComponent as ChatIcon } from '../../assets/outlined/chat.svg';
+import { ReactComponent as ChatIconActive } from '../../assets/filled/chat-filled.svg';
+import { ReactComponent as ProfileIcon } from '../../assets/outlined/profile.svg';
+import { ReactComponent as ProfileIconActive } from '../../assets/filled/profile-filled.svg';
 import { NavLink, useRouteMatch } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const BottomNavigationBar: React.FC = () => {
   return (
     <div className={styles.bottomNavigationBar}>
-      <ButtonLink label="Home" to="">
-        <HomeIcon />
+      <ButtonLink label="Home" to="/main">
+        {!useRouteMatch({ path: '/main', exact: true }) ? (
+          <HomeIcon />
+        ) : (
+          <HomeIconActive />
+        )}
       </ButtonLink>
-      <ButtonLink label="Discover" to="/discover">
-        <DiscoverIcon />
+      <ButtonLink label="Discover" to="/main/discover">
+        {!useRouteMatch('/main/discover') ? (
+          <DiscoverIcon />
+        ) : (
+          <DiscoverIconActive />
+        )}
       </ButtonLink>
-      <ButtonLink label="Chat" to="/chat">
-        <ChatIcon />
+      <ButtonLink label="Chat" to="/main/chat">
+        {!useRouteMatch('/main/chat') ? <ChatIcon /> : <ChatIconActive />}
       </ButtonLink>
-      <ButtonLink label="Profile" to="/profile">
-        <ProfileIcon />
+      <ButtonLink label="Profile" to="/main/profile">
+        {!useRouteMatch('/main/profile') ? (
+          <ProfileIcon />
+        ) : (
+          <ProfileIconActive />
+        )}
       </ButtonLink>
     </div>
   );
@@ -29,7 +45,6 @@ const BottomNavigationBar: React.FC = () => {
 type LinkProps = {
   label: string;
   to: string;
-  activeOnlyWhenExact?: boolean;
   children: PropTypes.ReactNodeLike;
 };
 
@@ -38,16 +53,8 @@ const ButtonLink: FunctionComponent<LinkProps> = ({
   to,
   children,
 }: LinkProps) => {
-  const match = useRouteMatch();
-  if (!match) {
-    return <div>Error</div>;
-  }
   return (
-    <NavLink
-      exact={true}
-      activeClassName={styles.active}
-      to={`${match.url}${to}`}
-    >
+    <NavLink exact={true} activeClassName={styles.active} to={to}>
       <button>
         {children}
         <p>{label}</p>
