6a08b45ea2b08f99c7bc53910518b9b404e28899
Joonas Kauppinen <joonasm.kauppinen@gmail.com>
2019-10-31 09:25:17 +0200

    FIx: formatting warnings

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,17 @@
 import React, { FC } from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
 import LandingPage from '../pages/LandingPage';
 import MainAppPage from '../pages/MainAppPage';
 
-import {isSession} from '../utils/apicall'
+import { isSession } from '../utils/apicall';
 
 const App: FC = () => {
-  console.log(`session: ${isSession()}`)
+  console.log(`session: ${isSession()}`);
   return (
     <Router basename="/project-mirror">
       <div>
@@ -21,31 +26,43 @@ const App: FC = () => {
             </li>
           </ul>
         </nav> */}
-  
+
         <Switch>
-          
-          <Route path="/main" render={(props) => (
-            isSession() === true
-            ? <MainAppPage />
-            : <Redirect to={{
-              pathname: '',
-              state: { from: props.location }
-            }} />
-          )} />
-          
-          <Route path="/" render={(props) => (
-            isSession() === false
-            ? <LandingPage />
-            : <Redirect to={{
-              pathname: '/main',
-              state: { from: props.location}
-            }} />
-          )} />
-          
+          <Route
+            path="/main"
+            render={props =>
+              isSession() === true ? (
+                <MainAppPage />
+              ) : (
+                <Redirect
+                  to={{
+                    pathname: '',
+                    state: { from: props.location },
+                  }}
+                />
+              )
+            }
+          />
+
+          <Route
+            path="/"
+            render={props =>
+              isSession() === false ? (
+                <LandingPage />
+              ) : (
+                <Redirect
+                  to={{
+                    pathname: '/main',
+                    state: { from: props.location },
+                  }}
+                />
+              )
+            }
+          />
         </Switch>
       </div>
     </Router>
-  )
+  );
 };
 
 export default App;
