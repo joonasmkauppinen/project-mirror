2adecc001f53a775b40707e70461a6601c2292f0
Niklas Kiuru <niklas.kiuru1@gmail.com>
2019-11-06 12:24:37 +0200

    Refactor: Hide tab pages instead of re-rendering them. Close #85

--- src/pages/MainAppPage/HomeTab/HomeTab.js
+++ src/pages/MainAppPage/HomeTab/HomeTab.js
@@ -6,21 +6,42 @@ import styles from '../../PageContainer.module.scss';
 const HomeTab = () => (
   <div className={styles.container}>
     <Header>Home tab</Header>
-    <Text>This is the home tab</Text>
-    <Text>This is the home tab</Text>
-    <Text>This is the home tab</Text>
-    <Text>This is the home tab</Text>
-    <Text>This is the home tab</Text>
-    <Text>This is the home tab</Text>
-    <Text>This is the home tab</Text>
-    <Text>This is the home tab</Text>
-    <Text>This is the home tab</Text>
-    <Text>This is the home tab</Text>
-    <Text>This is the home tab</Text>
-    <Text>This is the home tab</Text>
-    <Text>This is the home tab</Text>
-    <Text>This is the home tab</Text>
-    <Text>This is the home tab</Text>
+    <Text>1 This is the home tab</Text>
+    <Text>2 This is the home tab</Text>
+    <Text>3 This is the home tab</Text>
+    <Text>4 This is the home tab</Text>
+    <Text>5 This is the home tab</Text>
+    <Text>6 This is the home tab</Text>
+    <Text>7 This is the home tab</Text>
+    <Text>8 This is the home tab</Text>
+    <Text>9 This is the home tab</Text>
+    <Text>10 This is the home tab</Text>
+    <Text>11 This is the home tab</Text>
+    <Text>12 This is the home tab</Text>
+    <Text>13 This is the home tab</Text>
+    <Text>14 This is the home tab</Text>
+    <Text>15 This is the home tab</Text>
+    <Text>16 This is the home tab</Text>
+    <Text>17 This is the home tab</Text>
+    <Text>18 This is the home tab</Text>
+    <Text>19 This is the home tab</Text>
+    <Text>20 This is the home tab</Text>
+    <Text>21 This is the home tab</Text>
+    <Text>22 This is the home tab</Text>
+    <Text>23 This is the home tab</Text>
+    <Text>24 This is the home tab</Text>
+    <Text>25 This is the home tab</Text>
+    <Text>26 This is the home tab</Text>
+    <Text>27 This is the home tab</Text>
+    <Text>28 This is the home tab</Text>
+    <Text>29 This is the home tab</Text>
+    <Text>30 This is the home tab</Text>
+    <Text>31 This is the home tab</Text>
+    <Text>32 This is the home tab</Text>
+    <Text>33 This is the home tab</Text>
+    <Text>34 This is the home tab</Text>
+    <Text>35 This is the home tab</Text>
+    <Text>36 This is the home tab</Text>
   </div>
 );
 
--- src/pages/MainAppPage/MainAppPage.js
+++ src/pages/MainAppPage/MainAppPage.js
@@ -1,35 +1,41 @@
 import React from 'react';
 import styles from '../PageContainer.module.scss';
-import { Route, Switch, useRouteMatch } from 'react-router';
+import { useRouteMatch, useLocation } from 'react-router';
 import HomeTab from './HomeTab';
 import DiscoverTab from './DiscoverTab';
 import ChatTab from './ChatTab';
 import ProfileTab from './ProfileTab';
 import BottomNavigationBar from '../../components/BottomNavigationBar';
 
+const hidePage = (url, match, loc) => {
+  return loc.pathname !== `${match.url}${url}`;
+};
+
 const MainAppPage = () => {
   const match = useRouteMatch();
-  if (!match) {
-    return <div>Error</div>;
-  }
+  const loc = useLocation();
   return (
     <div>
       <div className={styles.tabContainer}>
         {/*<div className={styles.subContainer}>
           <Toolbar title="" info="true" />
   </div>*/}
-        <div className={styles.subContainer}>
-          <BottomNavigationBar className={styles.bottom} />
-          <div className={styles.content}>
-            <Switch>
-              <Route exact path={`${match.url}`} component={HomeTab} />
-              <Route path={`${match.url}/discover`} component={DiscoverTab} />
-              <Route path={`${match.url}/chat`} component={ChatTab} />
-              <Route path={`${match.url}/profile`} component={ProfileTab} />
-            </Switch>
+        <div className={styles.content}>
+          <div className={hidePage('', match, loc) && styles.hidden}>
+            <HomeTab />
+          </div>
+          <div className={hidePage('/discover', match, loc) && styles.hidden}>
+            <DiscoverTab />
+          </div>
+          <div className={hidePage('/chat', match, loc) && styles.hidden}>
+            <ChatTab />
+          </div>
+          <div className={hidePage('/profile', match, loc) && styles.hidden}>
+            <ProfileTab />
           </div>
         </div>
       </div>
+      <BottomNavigationBar />
     </div>
   );
 };
