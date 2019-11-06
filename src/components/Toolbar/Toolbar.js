4bde1fd02de29e7f476874a90487f341c8c17879
Joonas Kauppinen <joonasm.kauppinen@gmail.com>
2019-11-06 11:05:13 +0200

    Refactor: Toolbar

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,42 +1,42 @@
 import React from 'react';
 import styles from './Toolbar.module.scss';
 import IconButton from '../IconButton';
-import { ReactComponent as GoBackIcon } from '../../assets/outlined/back.svg';
-import { ReactComponent as InfoIcon } from '../../assets/outlined/info.svg';
 import PropTypes from 'prop-types';
 
-const todo = () => {
-  alert('todo!');
-};
-
-const goBack = () => {
-  window.history.back();
-};
-
-const Toolbar = ({ title, back = true, info = false }) => {
+const Toolbar = ({
+  title,
+  leftIcon,
+  rightIcon,
+  leftIconClick,
+  rightIconClick,
+}) => {
   return (
     <div className={styles.toolbar}>
-      <div className={styles.start}>
-        {back === true && <IconButton Icon={GoBackIcon} onClick={goBack} />}
-      </div>
+      {leftIcon && (
+        <IconButton
+          icon={leftIcon}
+          onClick={leftIconClick}
+          superClass={styles.toolbarIcon}
+        />
+      )}
       <div className={styles.title}>{title}</div>
-      <div className={styles.end}>
-        {info === true && (
-          <IconButton
-            Icon={InfoIcon}
-            onClick={todo}
-            className={styles.toRight}
-          />
-        )}
-      </div>
+      {rightIcon && (
+        <IconButton
+          icon={rightIcon}
+          onClick={rightIconClick}
+          superClass={styles.toolbarIcon}
+        />
+      )}
     </div>
   );
 };
 
 Toolbar.propTypes = {
   title: PropTypes.string,
-  back: PropTypes.bool,
-  info: PropTypes.bool,
+  leftIcon: PropTypes.string,
+  rightIcon: PropTypes.string,
+  leftIconClick: PropTypes.func,
+  rightIconClick: PropTypes.func,
 };
 
 export default Toolbar;
