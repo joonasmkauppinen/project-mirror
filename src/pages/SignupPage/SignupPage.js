3720e11e67040c5886216a6a49fb09a7d504de68
Joonas Kauppinen <joonasm.kauppinen@gmail.com>
2019-11-07 23:01:19 +0200

    Add: title to SignupPage and dictionary

--- src/pages/SignupPage/SignupPage.js
+++ src/pages/SignupPage/SignupPage.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import D from '../../utils/dictionary';
+import { t } from '../../utils/translate';
 
 import PageContainer from '../../hoc/PageContainer';
 import Toolbar from '../../components/Toolbar';
 
 const SignupPage = () => {
   const history = useHistory();
+  const title = t(D.SIGNUP.title);
   const handleLeftIconPress = () => history.goBack();
   return (
     <PageContainer>
-      <Toolbar leftIcon="back" onLeftIconClick={handleLeftIconPress} />
+      <Toolbar
+        title={title}
+        leftIcon="back"
+        onLeftIconClick={handleLeftIconPress}
+      />
       <div>dis will be signup page</div>
     </PageContainer>
   );
