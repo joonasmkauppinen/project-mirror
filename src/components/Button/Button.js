de318ff47e39eef011c1cf8fe344c9c7ec6bc0a3
Niklas Kiuru <niklas.kiuru1@gmail.com>
2019-11-07 10:52:29 +0200

    Add: disabled state & style to button component, Close #93

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,13 +2,14 @@ import React from 'react';
 import styles from './Button.module.scss';
 import PropTypes from 'prop-types';
 
-const Button = ({ label, superClass, onClick, secondary, style, id }) => {
+const Button = ({ label, superClass, onClick, secondary, style, id, disabled }) => {
   const classes = [styles.button];
   secondary ? classes.push(styles.secondary) : classes.push(styles.primary);
+  disabled && classes.push(styles.disabled);
   superClass && classes.push(superClass);
 
   return (
-    <div id={id} className={classes.join(' ')} onClick={onClick} style={style}>
+    <div id={id} className={classes.join(' ')} onClick={!disabled && onClick} style={style}>
       <p>{label}</p>
     </div>
   );
@@ -21,6 +22,7 @@ Button.propTypes = {
   superClass: PropTypes.string,
   label: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
 };
 
 export default Button;
