c78ceb36914d3e10e8b4346d6ad8edb785485848
Joonas Kauppinen <joonasm.kauppinen@gmail.com>
2019-11-07 17:09:35 +0200

    Update: use PageContainer in MainAppPage

--- src/pages/MainAppPage/MainAppPage.js
+++ src/pages/MainAppPage/MainAppPage.js
@@ -1,46 +1,29 @@
 import React from 'react';
-import styles from '../PageContainer.module.scss';
-import { useRouteMatch, useLocation } from 'react-router';
+import { useLocation } from 'react-router';
+import PageContainer from '../../hoc/PageContainer';
 import HomeTab from './HomeTab';
 import DiscoverTab from './DiscoverTab';
 import ChatTab from './ChatTab';
 import ProfileTab from './ProfileTab';
 import BottomNavigationBar from '../../components/BottomNavigationBar';
 
-const hidePage = (url, match, loc) => {
-  return loc.pathname !== `${match.url}${url}`;
-};
-
 const MainAppPage = () => {
-  const match = useRouteMatch();
-  const loc = useLocation();
+  const location = useLocation();
+
+  const [currentTab] = location.pathname.split('/').reverse();
+  const homeVisible = currentTab === 'main';
+  const discoverVisible = currentTab === 'discover';
+  const chatVisible = currentTab === 'chat';
+  const profileVisible = currentTab === 'profile';
+
   return (
-    <>
-      <div className={styles.tabContainer}>
-        {/*<div className={styles.subContainer}>
-          <Toolbar title="" info="true" />
-  </div>*/}
-        <div className={styles.content}>
-          <div className={hidePage('', match, loc) ? styles.hidden : ''}>
-            <HomeTab />
-          </div>
-          <div
-            className={hidePage('/discover', match, loc) ? styles.hidden : ''}
-          >
-            <DiscoverTab />
-          </div>
-          <div className={hidePage('/chat', match, loc) ? styles.hidden : ''}>
-            <ChatTab />
-          </div>
-          <div
-            className={hidePage('/profile', match, loc) ? styles.hidden : ''}
-          >
-            <ProfileTab />
-          </div>
-        </div>
-      </div>
+    <PageContainer>
+      <HomeTab visible={homeVisible} />
+      <DiscoverTab visible={discoverVisible} />
+      <ChatTab visible={chatVisible} />
+      <ProfileTab visible={profileVisible} />
       <BottomNavigationBar />
-    </>
+    </PageContainer>
   );
 };
 
