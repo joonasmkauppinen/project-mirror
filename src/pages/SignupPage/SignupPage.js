b6ffa5abc5d6f1fae88e4881cb05635b40a374fb
Joonas Kauppinen <joonasm.kauppinen@gmail.com>
2019-11-07 22:56:57 +0200

    Add: base structure for SignupPage

--- src/pages/SignupPage/SignupPage.js
+++ src/pages/SignupPage/SignupPage.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
-const SignupPage = () => <div>lel</div>;
+import PageContainer from '../../hoc/PageContainer';
+import Toolbar from '../../components/Toolbar';
+
+const SignupPage = () => {
+  const history = useHistory();
+  const handleLeftIconPress = () => history.goBack();
+  return (
+    <PageContainer>
+      <Toolbar leftIcon="back" onLeftIconClick={handleLeftIconPress} />
+      <div>dis will be signup page</div>
+    </PageContainer>
+  );
+};
 
 export default SignupPage;
