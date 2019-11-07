52be91187a51202dafaf8b7c5e72bf1614d297b7
Niklas Kiuru <niklas.kiuru1@gmail.com>
2019-11-07 10:59:12 +0200

    Fix: conditional onClick bug

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,14 +2,27 @@ import React from 'react';
 import styles from './Button.module.scss';
 import PropTypes from 'prop-types';
 
-const Button = ({ label, superClass, onClick, secondary, style, id, disabled }) => {
+const Button = ({
+  label,
+  superClass,
+  onClick,
+  secondary,
+  style,
+  id,
+  disabled,
+}) => {
   const classes = [styles.button];
   secondary ? classes.push(styles.secondary) : classes.push(styles.primary);
   disabled && classes.push(styles.disabled);
   superClass && classes.push(superClass);
 
   return (
-    <div id={id} className={classes.join(' ')} onClick={!disabled && onClick} style={style}>
+    <div
+      id={id}
+      className={classes.join(' ')}
+      onClick={!disabled ? onClick : undefined}
+      style={style}
+    >
       <p>{label}</p>
     </div>
   );
