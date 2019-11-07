4287fdd5c6c77abe79322a731045e62821c416e8
Niklas Kiuru <niklas.kiuru1@gmail.com>
2019-11-07 15:59:57 +0200

    Add: toggle to discover tab

--- src/pages/MainAppPage/DiscoverTab/DiscoverTab.js
+++ src/pages/MainAppPage/DiscoverTab/DiscoverTab.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../../components/Header';
 import Text from '../../../components/Text';
 import styles from '../../PageContainer.module.scss';
 import IconButton from '../../../components/IconButton';
+import Toggle from '../../../components/Toggle';
 
 const test = () => {
   console.log('Test');
 };
 
-const DiscoverTab = () => (
-  <div className={styles.container}>
-    <Header>Discover</Header>
-    <Text>This is the discover tab</Text>
-    <IconButton onClick={test} icon={'info'} />
-  </div>
-);
+const DiscoverTab = () => {
+  const [value, setValue] = useState(false);
+  return (
+    <div className={styles.container}>
+      <Header>Discover</Header>
+      <Text>This is the discover tab</Text>
+      <IconButton onClick={test} icon={'info'} />
+      <Toggle isOn={value} handleToggle={() => setValue(!value)} />
+    </div>
+  );
+};
 
 export default DiscoverTab;
