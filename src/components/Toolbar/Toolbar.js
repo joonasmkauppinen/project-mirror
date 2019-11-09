beed018fb1273a90fa4a3ee8780030924ddb1fee
Niklas Kiuru <niklas.kiuru1@gmail.com>
2019-11-09 09:45:17 +0200

    Fix: disable touchmove on toolbar

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import styles from './Toolbar.module.scss';
 import IconButton from '../IconButton';
 import PropTypes from 'prop-types';
@@ -9,25 +9,33 @@ const Toolbar = ({
   rightIcon,
   onLeftIconClick,
   onRightIconClick,
-}) => (
-  <div className={styles.toolbar}>
-    {leftIcon && (
-      <IconButton
-        icon={leftIcon}
-        onClick={onLeftIconClick}
-        superClass={styles.toolbarIcon}
-      />
-    )}
-    <div className={styles.title}>{title}</div>
-    {rightIcon && (
-      <IconButton
-        icon={rightIcon}
-        onClick={onRightIconClick}
-        superClass={styles.toolbarIcon}
-      />
-    )}
-  </div>
-);
+}) => {
+  const toolbar = useRef(null);
+  useEffect(() => {
+    toolbar.current.addEventListener('touchmove', e => {
+      e.preventDefault();
+    });
+  }, []);
+  return (
+    <div className={styles.toolbar} ref={toolbar}>
+      {leftIcon && (
+        <IconButton
+          icon={leftIcon}
+          onClick={onLeftIconClick}
+          superClass={styles.toolbarIcon}
+        />
+      )}
+      <div className={styles.title}>{title}</div>
+      {rightIcon && (
+        <IconButton
+          icon={rightIcon}
+          onClick={onRightIconClick}
+          superClass={styles.toolbarIcon}
+        />
+      )}
+    </div>
+  );
+};
 
 Toolbar.propTypes = {
   title: PropTypes.string,
