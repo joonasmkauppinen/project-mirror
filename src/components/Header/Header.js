7d339872ad8f6501fe74e36c73cca24e30384e6a
Niklas Kiuru <niklas.kiuru1@gmail.com>
2019-11-11 21:09:45 +0200

    Fix: div being nested in a p tag

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Header.module.scss';
 
-const Header = ({ children }) => <p className={styles.header}>{children}</p>;
+const Header = ({ children }) => <div className={styles.header}>{children}</div>;
 
 Header.propTypes = {
   children: PropTypes.oneOfType([
