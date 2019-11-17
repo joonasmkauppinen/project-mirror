f8b2a2bf129418d2f514015d06a41aa285b9dd64
Joonas Kauppinen <joonas.kauppinen@yle.fi>
2019-11-17 19:27:28 +0200

    Remove: 'id' prop from Card, Dialog doesn't require it anymore

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,18 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Card.module.scss';
 
-const Card = ({ children, id, style, superClass }) => {
+const Card = ({ children, style, superClass }) => {
   const classes = [styles.card];
   superClass && classes.push(superClass);
   return (
-    <div id={id} style={style} className={classes.join(' ')}>
+    <div style={style} className={classes.join(' ')}>
       {children}
     </div>
   );
 };
 
 Card.propTypes = {
-  id: PropTypes.string,
   children: PropTypes.node.isRequired,
   style: PropTypes.object,
   superClass: PropTypes.string,
