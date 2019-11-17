7bc32b77b0970619222430e78c8ca0e3c34ab286
Joonas Kauppinen <joonas.kauppinen@yle.fi>
2019-11-17 20:15:36 +0200

    Add: action buttons, render children and handle clicks

--- src/components/Dialog/Dialog.js
+++ src/components/Dialog/Dialog.js
@@ -2,15 +2,26 @@ import React from 'react';
 import { CSSTransition } from 'react-transition-group';
 import styles from './Dialog.module.scss';
 import Card from '../Card';
-import Text from '../Text';
 import fade from '../../animation/dialogBgFadeAnimation.module.scss';
 import dialogReveal from '../../animation/dialogRevealAnimation.module.scss';
 
 import PropTypes from 'prop-types';
 
-const Dialog = ({ visible, onEnter, onExited, onOutsideClick }) => {
-  const handleBackgroundClick = ({ target }) =>
-    !(target.id === 'dialog') && onOutsideClick();
+const Dialog = ({
+  children,
+  visible,
+  header,
+  onEnter,
+  onExited,
+  onOutsideClick,
+  positiveLabel,
+  onPositiveClicked,
+  negativeLabel,
+  onNegativeClicked,
+}) => {
+  const handleBackgroundClick = ({ target }) => {
+    if (target.id === 'bg' && onOutsideClick) onOutsideClick();
+  };
 
   return (
     <CSSTransition
@@ -21,15 +32,34 @@ const Dialog = ({ visible, onEnter, onExited, onOutsideClick }) => {
       onEnter={onEnter}
       onExited={onExited}
     >
-      <div className={styles.container} onClick={handleBackgroundClick}>
+      <div id="bg" className={styles.container} onClick={handleBackgroundClick}>
         <CSSTransition
           in={visible}
           appear
           timeout={400}
           classNames={dialogReveal}
         >
-          <Card id="dialog" superClass={styles.dialog}>
-            <Text>This is a test Dialog.</Text>
+          <Card superClass={styles.dialog}>
+            <div className={styles.header}>{header}</div>
+            {children}
+            <div className={styles.buttonContainer}>
+              {onNegativeClicked && (
+                <div
+                  className={`${styles.button} ${styles.negative}`}
+                  onClick={onNegativeClicked}
+                >
+                  {negativeLabel}
+                </div>
+              )}
+              {onPositiveClicked && (
+                <div
+                  className={`${styles.button} ${styles.positive}`}
+                  onClick={onPositiveClicked}
+                >
+                  {positiveLabel}
+                </div>
+              )}
+            </div>
           </Card>
         </CSSTransition>
       </div>
@@ -38,10 +68,16 @@ const Dialog = ({ visible, onEnter, onExited, onOutsideClick }) => {
 };
 
 Dialog.propTypes = {
+  children: PropTypes.node.isRequired,
   visible: PropTypes.bool.isRequired,
+  header: PropTypes.string,
   onEnter: PropTypes.func,
   onExited: PropTypes.func,
   onOutsideClick: PropTypes.func,
+  positiveLabel: PropTypes.string,
+  onPositiveClicked: PropTypes.func,
+  negativeLabel: PropTypes.string,
+  onNegativeClicked: PropTypes.func,
 };
 
 export default Dialog;
--- src/pages/MainAppPage/HomeTab/HomeTab.js
+++ src/pages/MainAppPage/HomeTab/HomeTab.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Header from '../../../components/Header';
 import TabContainer from '../../../hoc/TabContainer';
@@ -7,24 +7,44 @@ import styles from './HomeTab.module.scss';
 import TabTitle from '../../../components/TabTitle';
 import IconButton from '../../../components/IconButton';
 import TaskList from '../../../components/TasksList';
+import Dialog from '../../../components/Dialog/Dialog';
+import Button from '../../../components/Button';
+import Text from '../../../components/Text';
 
-const HomeTab = ({ visible }) => (
-  <TabContainer active={visible}>
-    <TabTitle>
-      <Header>
-        <div className={styles.headerLight}>{t('HOME.title')}</div>
-        {'\n'}
-        User
-      </Header>
-      <IconButton
-        icon={'info'}
-        onClick={() => {}}
-        style={{ alignSelf: 'flex-start' }}
-      />
-    </TabTitle>
-    <TaskList />
-  </TabContainer>
-);
+const HomeTab = ({ visible }) => {
+  const [showDialog, setShowDialog] = useState(false);
+  return (
+    <TabContainer active={visible}>
+      <TabTitle>
+        <Header>
+          <div className={styles.headerLight}>{t('HOME.title')}</div>
+          {'\n'}
+          User
+        </Header>
+        <IconButton
+          icon={'info'}
+          onClick={() => {}}
+          style={{ alignSelf: 'flex-start' }}
+        />
+      </TabTitle>
+      <TaskList />
+      <Button label="Show test dialog" onClick={() => setShowDialog(true)} />
+      <Dialog
+        header="Test dialog"
+        visible={showDialog}
+        onOutsideClick={() => setShowDialog(false)}
+        positiveLabel="done"
+        negativeLabel="close"
+        onNegativeClicked={() => setShowDialog(false)}
+      >
+        <Text>
+          This is just a test dialog and should be removed from HomeTab after
+          testing.
+        </Text>
+      </Dialog>
+    </TabContainer>
+  );
+};
 
 HomeTab.propTypes = {
   visible: PropTypes.bool,
