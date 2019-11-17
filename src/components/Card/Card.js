a91e5f9529ffe81697bfd5efbbaa507bd6d59359
Joonas Kauppinen <joonas.kauppinen@yle.fi>
2019-11-17 18:55:50 +0200

    Add: 'id' prop into Card for use in Dialog

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,17 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Card.module.scss';
 
-const Card = ({ children, style, superClass }) => {
+const Card = ({ children, id, style, superClass }) => {
   const classes = [styles.card];
   superClass && classes.push(superClass);
   return (
-    <div style={style} className={classes.join(' ')}>
+    <div id={id} style={style} className={classes.join(' ')}>
       {children}
     </div>
   );
 };
 
 Card.propTypes = {
+  id: PropTypes.string,
   children: PropTypes.node.isRequired,
   style: PropTypes.object,
   superClass: PropTypes.string,
