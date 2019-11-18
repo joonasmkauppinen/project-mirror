74bb6c2000bd20e4fae3b2a48c51485a9fac1a11
Niklas Kiuru <niklas.kiuru1@gmail.com>
2019-11-18 21:31:09 +0200

    Update: gauge index.js, Closes #153

--- src/components/Gauges/index.js
+++ src/components/Gauges/index.js
@@ -1,17 +1,2 @@
 import Gauge from './Gauge';
-import { connect } from 'react-redux';
-import { pick } from 'lodash-es';
-import { loadGauges } from '../../store/gauges/actions';
-
-function mapStateToProps(state) {
-  return pick(state.gauges, ['gauges', 'loading', 'error']);
-}
-
-const mapDispatchToProps = {
-  loadGauges,
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(Gauge);
+export default Gauge;
