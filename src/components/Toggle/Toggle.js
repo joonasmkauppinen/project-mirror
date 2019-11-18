5cd375ffc2ebf70fdae4d989a45a2c910accf361
Niklas Kiuru <niklas.kiuru1@gmail.com>
2019-11-18 13:08:58 +0200

    Update: toggle to use uuid

--- src/components/Toggle/Toggle.js
+++ src/components/Toggle/Toggle.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import styles from './Toggle.module.scss';
 import PropTypes from 'prop-types';
+import { uniqueId } from 'lodash-es';
 
 const Toggle = ({ isOn, handleToggle }) => {
+  const id = uniqueId('toggle');
   return (
     <div className={styles.toggle}>
       <input
         checked={isOn}
         onChange={handleToggle}
         className={styles.checkbox}
-        id={`toggle`}
+        id={id}
         type="checkbox"
       />
       <label
         className={styles.toggleLabel + ' ' + (isOn ? styles.active : '')}
-        htmlFor={`toggle`}
+        htmlFor={id}
       >
         <span className={styles.toggleButton} />
       </label>
