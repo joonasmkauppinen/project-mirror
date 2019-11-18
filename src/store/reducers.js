a9119aa68084ac74c96e0559b4f48011c916bc69
Niklas Kiuru <niklas.kiuru1@gmail.com>
2019-11-18 09:42:26 +0200

    Add: user gauges to redux, #126

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import organizations from './organizations/reducers';
 import tasks from './tasks/reducers';
 import posts from './posts/reducers';
+import gauges from './gauges/reducers';
 
 export default combineReducers({
   organizations,
   tasks,
   posts,
+  gauges,
 });
