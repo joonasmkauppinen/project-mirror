0d7146fcad4fbb30493c2ecf8e1612711229c16f
Niklas Kiuru <niklas.kiuru1@gmail.com>
2019-11-20 13:54:11 +0200

    Update: post styles, Closes #159

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -1,33 +1,50 @@
 import React from 'react';
 import styles from './PostList.module.scss';
 import PropTypes from 'prop-types';
-import Text from '../Text';
 import LoadingIndicator from '../LoadingIndicator';
-import Button from '../Button';
 import Subheader from '../Subheader';
+import Card from '../Card';
+import Icons from '../../assets/Icons';
 
 // eslint-disable-next-line no-unused-vars
 const PostList = ({ posts, loading, error }) => {
+  console.log(posts);
   return (
     <>
       <Subheader>Posts</Subheader>
       <LoadingIndicator loading={false} error={error}>
-        {posts.map(post => (
-          <div key={post.id}>
-            <div className={styles.postsContainer}>
-              <Text>{post.teaser}</Text>
-              <div>
-                <Button
-                  label={'action'}
-                  onClick={() => {
-                    console.log(post);
-                  }}
-                  style={{ width: '80px' }}
-                />
-              </div>
+        <div className={styles.postsContainer}>
+          {posts.map(post => (
+            <div
+              key={post.id}
+              className={styles.post}
+              onClick={() => {
+                console.log('clicc');
+              }}
+            >
+              <Card>
+                <div className={styles.postHeader}>
+                  <div className={styles.postTitle}>
+                    <Icons.peili />
+                    <p>{post.organization}</p>
+                    <Icons.peiliFilled />
+                  </div>
+                </div>
+                {post.image && (
+                  <img
+                    className={styles.postImage}
+                    src={post.image}
+                    alt="post"
+                  />
+                )}
+                <div className={styles.postContent}>
+                  <div className={styles.time}>7 Years ago</div>
+                  <div className={styles.teaser}>{post.teaser}</div>
+                </div>
+              </Card>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </LoadingIndicator>
     </>
   );
