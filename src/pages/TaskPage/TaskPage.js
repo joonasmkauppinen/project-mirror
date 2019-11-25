8842443fbdc68d72bb562e7d41be087ec8ce94aa
Joonas Kauppinen <joonas.kauppinen@yle.fi>
2019-11-25 21:13:06 +0200

    Add: QuestionHeader

--- src/pages/TaskPage/TaskPage.js
+++ src/pages/TaskPage/TaskPage.js
@@ -1,27 +1,36 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
-import D from '../../utils/dictionary';
-import { t } from '../../utils/translate';
+// import D from '../../utils/dictionary';
+// import { t } from '../../utils/translate';
 import PageContainer from '../../hoc/PageContainer';
 import Toolbar from '../../components/Toolbar';
 import ScrollableContent from '../../hoc/ScrollableContent';
 import styles from './TaskPage.module.scss';
 import Text from '../../components/Text';
-import Header from '../../components/Header';
+import Header from '../../components/QuestionHeader';
 import Button from '../../components/Button';
-import { taskInit } from '../../utils/task';
+import { taskInit, taskGetAllQuestionsData } from '../../utils/task';
 
 const TaskPage = () => {
-  const [progress, setProgress] = useState(0);
+  const [progress] = useState(0);
   const second = useRef(null);
   const { id } = useParams();
   const { goBack } = useHistory();
-  const startButtonLabel = t(D.QUESTIONNAIRE.start);
+
+  useEffect(() => {
+    const fetchTask = async () => {
+      const response = await taskInit(id);
+      console.log(response);
+      console.log(taskGetAllQuestionsData());
+    };
+    fetchTask();
+  }, []);
 
   const handleRightIconClick = () => alert('TODO: show task info');
   const buttonClick = () => {
     second.current.scrollIntoView({ behavior: 'smooth' });
   };
+
   return (
     <PageContainer>
       <Toolbar
@@ -32,31 +41,17 @@ const TaskPage = () => {
         onRightIconClick={handleRightIconClick}
       />
       <div className={styles.root}>
-        <section className={styles.fillScreen}>
-          <ScrollableContent>
-            <div
-              style={{
-                marginTop: '64px',
-                flex: '1',
-              }}
-            >
-              <Header>{id}</Header>
-              <Text>Questionnaire description is going to be here.</Text>
-            </div>
-            <Button
-              style={{ marginBottom: '64px' }}
-              label={startButtonLabel}
-              onClick={buttonClick}
-            />
-          </ScrollableContent>
-        </section>
-        <section ref={second} className={styles.fillScreen}>
-          <ScrollableContent>
-            <Header>Question 2 here</Header>
-            <Text>This is not suppose to be here.</Text>
-          </ScrollableContent>
-          <Button label="this will the next button" onClick={buttonClick} />
-        </section>
+        {taskGetAllQuestionsData().map(({ id, prompt, options }) => {
+          return (
+            <section key={id} className={styles.fillScreen}>
+              <ScrollableContent>
+                <Header>{prompt}</Header>
+                <Text>{JSON.stringify(options, null, 2)}</Text>
+              </ScrollableContent>
+              <Button label="this will the next button" onClick={buttonClick} />
+            </section>
+          );
+        })}
       </div>
     </PageContainer>
   );
