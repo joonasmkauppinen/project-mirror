8678e3705007b18d8ca44d7471172731bbdbf3af
Samuli Virtanen <samuli.virtanen@tucom.fi>
2019-11-25 21:02:11 +0200

    removed task tests from chat page

--- src/pages/MainAppPage/ChatTab/ChatTab.js
+++ src/pages/MainAppPage/ChatTab/ChatTab.js
@@ -7,59 +7,14 @@ import { t } from '../../../utils/translate';
 import TabTitle from '../../../components/TabTitle';
 import IconButton from '../../../components/IconButton';
 
-import {
-  taskInit,
-  taskGetTaskQuestionsCount,
-  taskGetCurrentQuestionData,
-  taskAnswerToCurrent,
-  taskGetCurrentQuestionIndex,
-  taskGetPercentageProgress,
-} from '../../../utils/task';
-
 const ChatTab = ({ visible }) => {
-  taskInit(2).then(response => {
-    console.log('Task is initialized');
-    console.log(response);
-    ref();
-  });
-
-  const ref = () => {
-    try {
-      document.getElementById('q1').innerHTML =
-        taskGetCurrentQuestionIndex() + 1;
-      document.getElementById('q2').innerHTML = taskGetTaskQuestionsCount();
-      document.getElementById('perc').innerHTML = taskGetPercentageProgress();
-      const qData = taskGetCurrentQuestionData();
-      document.getElementById('qq').innerHTML = qData.prompt;
-      console.log('Current Question:');
-      console.log(taskGetCurrentQuestionData());
-    } catch (e) {
-      //
-    }
-  };
-
-  const gonext = () => {
-    const answer = document.getElementById('ans').value;
-    taskAnswerToCurrent(answer);
-    ref();
-  };
-
   return (
     <TabContainer active={visible}>
       <TabTitle>
         <Header>{t('TABS.chat')}</Header>
         <IconButton icon={'robot'} onClick={() => {}} />
       </TabTitle>
-      <Text>
-        <span id="qq">This is the chat tab</span>
-      </Text>
-      <Text>
-        Question: <span id="q1">#</span>/<span id="q2">#</span>
-        <br />
-        Percent: <span id="perc">0</span> %
-      </Text>
-      <input type="text" id="ans" />
-      <input type="button" onClick={gonext} value="answer" />
+      <Text>This is the chat tab</Text>
     </TabContainer>
   );
 };
