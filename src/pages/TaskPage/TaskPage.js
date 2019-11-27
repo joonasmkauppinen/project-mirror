28ced45401707edad94abf7071e5fdeeff955b09
Samuli Virtanen <samuli.virtanen@tucom.fi>
2019-11-27 23:36:53 +0200

    integration with backend

--- src/pages/TaskPage/TaskPage.js
+++ src/pages/TaskPage/TaskPage.js
@@ -12,8 +12,8 @@ import LoadingSpinner from '../../components/LoadingSpinner';
 import {
   taskInit,
   taskGetAllQuestionsData,
-  taskGetCurrentQuestionIndex,
   taskGetPercentageProgress,
+  taskAnswerToQuestionByIndex,
 } from '../../utils/task';
 import NextButton from '../../components/QuestionNextButton';
 
@@ -34,11 +34,10 @@ const TaskPage = () => {
 
   const handleRightIconClick = () => alert('TODO: show task info');
   const buttonClick = () => {};
-  const handleOptionChange = (event, index) => {
+  const handleOptionChange = (_event, index, value) => {
     console.log('q index: ', index);
-    const value = event.target.value;
     console.log(value);
-    taskGetCurrentQuestionIndex(index, value);
+    taskAnswerToQuestionByIndex(index, value);
     const newProgress = taskGetPercentageProgress();
     console.log('new progress: ', newProgress);
     setProgress(taskGetPercentageProgress);
@@ -57,33 +56,43 @@ const TaskPage = () => {
         <LoadingSpinner />
       ) : (
         <div className={styles.root}>
-          {taskGetAllQuestionsData().map(({ id, prompt, options }, qIndex) => {
-            console.log('options: ', options);
-            const values = Object.values(options);
-            console.log('values: ', values);
-            return (
-              <section key={id} className={styles.fillScreen}>
-                <ScrollableContent>
-                  <Header>{prompt}</Header>
-                  {values.map(({ value, mathValue }, oIndex) => {
-                    return (
-                      <label key={`q${qIndex}-${oIndex}`}>
-                        <input
-                          onChange={event => handleOptionChange(event, qIndex)}
-                          type="radio"
-                          name={`q${qIndex}`}
-                          value={mathValue}
-                        />
-                        <Text style={{ display: 'inline-block' }}>{value}</Text>
-                        <br />
-                      </label>
-                    );
-                  })}
-                </ScrollableContent>
-                <NextButton label="seuraava" onClick={buttonClick} />
-              </section>
-            );
-          })}
+          {taskGetAllQuestionsData().map(
+            ({ id, prompt, options, logicalPolarity }, qIndex) => {
+              console.log('options: ', options);
+              const values = Object.values(options);
+              console.log('values: ', values);
+              if (logicalPolarity) {
+                return (
+                  <section key={id} className={styles.fillScreen}>
+                    <ScrollableContent>
+                      <Header>{prompt}</Header>
+                      {values.map(({ value, id }, oIndex) => {
+                        return (
+                          <label key={`q${qIndex}-${oIndex}`}>
+                            <input
+                              onChange={event =>
+                                handleOptionChange(event, qIndex, id)
+                              }
+                              type="radio"
+                              name={`q${qIndex}`}
+                              value={value}
+                            />
+                            <Text style={{ display: 'inline-block' }}>
+                              {value}
+                            </Text>
+                            <br />
+                          </label>
+                        );
+                      })}
+                    </ScrollableContent>
+                    <NextButton label="seuraava" onClick={buttonClick} />
+                  </section>
+                );
+              } else {
+                return <></>;
+              }
+            },
+          )}
         </div>
       )}
     </PageContainer>
