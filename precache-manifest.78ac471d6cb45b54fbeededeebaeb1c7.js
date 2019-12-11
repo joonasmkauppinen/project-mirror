b5c9145eadf64c4145ed560660b26647c6101f20
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-11 13:14:52 +0000

    Deploy joonasmkauppinen/project-mirror to github.com/joonasmkauppinen/project-mirror.git:gh-pages

---
+++ precache-manifest.78ac471d6cb45b54fbeededeebaeb1c7.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c88e1e0e54e7c28ab6314fad86d6fdfc",
+    "url": "/project-mirror/index.html"
+  },
+  {
+    "revision": "a58dd3baaab31fd241e6",
+    "url": "/project-mirror/static/css/main.6c368c74.chunk.css"
+  },
+  {
+    "revision": "477b50f6a721ba6adeef",
+    "url": "/project-mirror/static/js/2.49b9f41e.chunk.js"
+  },
+  {
+    "revision": "a58dd3baaab31fd241e6",
+    "url": "/project-mirror/static/js/main.08e2bbe0.chunk.js"
+  },
+  {
+    "revision": "7dd793589f2b99defb73",
+    "url": "/project-mirror/static/js/runtime-main.916486d0.js"
+  },
+  {
+    "revision": "694bf2fb591e039d1450aabcefb54fd5",
+    "url": "/project-mirror/static/media/alert_ic.694bf2fb.svg"
+  },
+  {
+    "revision": "65b4579a610524a83464dd5a71291818",
+    "url": "/project-mirror/static/media/arrow-long-right.65b4579a.svg"
+  },
+  {
+    "revision": "07a70e4d0f07574991a940563895c3f5",
+    "url": "/project-mirror/static/media/avatar.07a70e4d.svg"
+  },
+  {
+    "revision": "08b866b4dee2e0faaf2792cf63f4e503",
+    "url": "/project-mirror/static/media/back.08b866b4.svg"
+  },
+  {
+    "revision": "ba8f4ba4223a2b27268ffb08f4f08646",
+    "url": "/project-mirror/static/media/chat - Copy.ba8f4ba4.svg"
+  },
+  {
+    "revision": "5cfa6d03a99583132dc02bc488c47ada",
+    "url": "/project-mirror/static/media/chat-filled.5cfa6d03.svg"
+  },
+  {
+    "revision": "7b14848ee922ba8ecdc0e37938ff6c4d",
+    "url": "/project-mirror/static/media/check_ic.7b14848e.svg"
+  },
+  {
+    "revision": "5f8707a4ffdf03a1c20d78b1b422e300",
+    "url": "/project-mirror/static/media/circle_check_ic.5f8707a4.svg"
+  },
+  {
+    "revision": "38ea019e7b3fd6e07166fd56eb0bae30",
+    "url": "/project-mirror/static/media/close_ic.38ea019e.svg"
+  },
+  {
+    "revision": "38ea019e7b3fd6e07166fd56eb0bae30",
+    "url": "/project-mirror/static/media/cross.38ea019e.svg"
+  },
+  {
+    "revision": "b60613179bd5af80bc2ef38fdc725cd4",
+    "url": "/project-mirror/static/media/discover-filled.b6061317.svg"
+  },
+  {
+    "revision": "66fbab0f9aeb8c757c85b664799b20c5",
+    "url": "/project-mirror/static/media/discover.66fbab0f.svg"
+  },
+  {
+    "revision": "3ee83cbc4eb967342813465ce23336e7",
+    "url": "/project-mirror/static/media/double_next.3ee83cbc.svg"
+  },
+  {
+    "revision": "157dfda203dcc0a27ab013a55a79279a",
+    "url": "/project-mirror/static/media/ellis-prototype_ic.157dfda2.svg"
+  },
+  {
+    "revision": "86adc1e4c47ddc7c7748638faa6869d0",
+    "url": "/project-mirror/static/media/home-filled.86adc1e4.svg"
+  },
+  {
+    "revision": "a62dc92478ec5d9f4676cb9c669cd7f9",
+    "url": "/project-mirror/static/media/home.a62dc924.svg"
+  },
+  {
+    "revision": "e65d9c9fbd819fafac2cbdd13fce4eef",
+    "url": "/project-mirror/static/media/info-filled.e65d9c9f.svg"
+  },
+  {
+    "revision": "893449e18e5ad298553af40933060fc2",
+    "url": "/project-mirror/static/media/info.893449e1.svg"
+  },
+  {
+    "revision": "92f476b5fd2959870735313df94de51e",
+    "url": "/project-mirror/static/media/language_ic.92f476b5.svg"
+  },
+  {
+    "revision": "ccf50494e6b2cf2766dc9c3b112a1535",
+    "url": "/project-mirror/static/media/level-up.ccf50494.svg"
+  },
+  {
+    "revision": "bdbd068ba58f9fbe5a65706df0c391ba",
+    "url": "/project-mirror/static/media/location-pin-filled_ic.bdbd068b.svg"
+  },
+  {
+    "revision": "1c22012b53213d0b163111f821470024",
+    "url": "/project-mirror/static/media/location-pin-outline_ic.1c22012b.svg"
+  },
+  {
+    "revision": "96bb3a76e27898c0ccc30925c8ce67f6",
+    "url": "/project-mirror/static/media/location_ic.96bb3a76.svg"
+  },
+  {
+    "revision": "60111bd0ff29f3667cbaa54ada9e29a0",
+    "url": "/project-mirror/static/media/notification_ic.60111bd0.svg"
+  },
+  {
+    "revision": "bdb617cc96e259c5387335fcf4186824",
+    "url": "/project-mirror/static/media/peili-official-logo-outline_ic.bdb617cc.svg"
+  },
+  {
+    "revision": "b0490a6f12a940e2f02c6395adf85553",
+    "url": "/project-mirror/static/media/peili-official-logo_ic.b0490a6f.svg"
+  },
+  {
+    "revision": "a8587560b312d5834391110ec98cca8e",
+    "url": "/project-mirror/static/media/phone_ic.a8587560.svg"
+  },
+  {
+    "revision": "bae92d1305d5aa10014e3ca64624a961",
+    "url": "/project-mirror/static/media/profile-filled.bae92d13.svg"
+  },
+  {
+    "revision": "9bb739acc0a49ec30b914f1f31cefde5",
+    "url": "/project-mirror/static/media/profile.9bb739ac.svg"
+  },
+  {
+    "revision": "73216653f6d9b293bfd6f218935b4279",
+    "url": "/project-mirror/static/media/resultCheckmark.73216653.svg"
+  },
+  {
+    "revision": "a2d908d86c3b16e56b458200f18fb34b",
+    "url": "/project-mirror/static/media/scroll-indicator.a2d908d8.svg"
+  },
+  {
+    "revision": "33bb747ee9bf6ef390ddb05f0f3ebdaf",
+    "url": "/project-mirror/static/media/search_ic.33bb747e.svg"
+  },
+  {
+    "revision": "345c48ee90b34479b7c3b4c08ac18c49",
+    "url": "/project-mirror/static/media/settings_ic.345c48ee.svg"
+  },
+  {
+    "revision": "9ce5b60c0ea3eca37a6e469276db5b24",
+    "url": "/project-mirror/static/media/trophy.9ce5b60c.svg"
+  }
+]);
